a552b8ecf7f04c8314c01b1c2ab73f50ba0bbafc
zimmu. <mzimmer@tktk.ee>
2023-09-21 19:49:39 +0300

    create new app

---
+++ myfirstapp/Program.cs
@@ -0,0 +1,13 @@
+﻿//this is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello {userName} !"); //Output
